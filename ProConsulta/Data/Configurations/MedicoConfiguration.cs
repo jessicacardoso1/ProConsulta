3e25814ede66b2c120b25a37c1c923e9d1ea1351
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-01-25 20:26:29 -0300

    Update medicoID

--- ProConsulta/Data/Configurations/MedicoConfiguration.cs
+++ ProConsulta/Data/Configurations/MedicoConfiguration.cs
@@ -36,7 +36,7 @@ namespace ProConsulta.Data.Configurations
 
             builder.HasMany(a => a.Agendamentos)
                 .WithOne(m => m.Medico)
-                .HasForeignKey(m => m.Medico.Id)
+                .HasForeignKey(m => m.MedicoId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
