ee84b9a78d97a4a143b67bac7f8068d5d8b8ee29
Jéssica Cardoso <jessica.cardosor@gmail.com>
2025-01-25 20:24:16 -0300

    Add Migrations

--- ProConsulta/Migrations/20250125220624_CreateInitial.cs
+++ ProConsulta/Migrations/20250125220624_CreateInitial.cs
@@ -3,10 +3,12 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace ProConsulta.Migrations
 {
     /// <inheritdoc />
-    public partial class CreateIdentitySchema : Migration
+    public partial class CreateInitial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -30,6 +32,8 @@ namespace ProConsulta.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(21)", maxLength: 21, nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
@@ -48,6 +52,37 @@ namespace ProConsulta.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Especialidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Descricao = table.Column<string>(type: "NVARCHAR(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especialidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Documento = table.Column<string>(type: "VARCHAR(11)", nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Celular = table.Column<string>(type: "VARCHAR(11)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -156,6 +191,105 @@ namespace ProConsulta.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    Documento = table.Column<string>(type: "NVARCHAR(11)", nullable: false),
+                    Crm = table.Column<string>(type: "NVARCHAR(8)", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Celular = table.Column<string>(type: "NVARCHAR(11)", nullable: false),
+                    EspecialidadeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medicos_Especialidades_EspecialidadeId",
+                        column: x => x.EspecialidadeId,
+                        principalTable: "Especialidades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Observacao = table.Column<string>(type: "VARCHAR(250)", nullable: false),
+                    PacienteId = table.Column<int>(type: "int", nullable: false),
+                    MedicoId = table.Column<int>(type: "int", nullable: false),
+                    HoraConsulta = table.Column<TimeSpan>(type: "time", nullable: false),
+                    DataConsulta = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Medicos_MedicoId",
+                        column: x => x.MedicoId,
+                        principalTable: "Medicos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Agendamentos_Pacientes_PacienteId",
+                        column: x => x.PacienteId,
+                        principalTable: "Pacientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "00043fbd-e5e1-49eb-8e36-837561d584f1", null, "Medico", "MEDICO" },
+                    { "8305f33b-5412-47d0-b4ab-17cf6867f2e2", null, "Atendente", "ATENDENTE" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Discriminator", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Nome", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "95433ac4-2fe9-468f-b80d-b05ec3724d1d", 0, "43afebca-21ed-4b3c-ab26-1b0a4d13f087", "Atendente", "proconsulta@hotmail.com.br", true, false, null, "Pro Consulta", "PROCONSULTA@HOTMAIL.COM.BR", "PROCONSULTA@HOTMAIL.COM.BR", "AQAAAAIAAYagAAAAEMYvZNowmoMuJSUuw53OpzfrL8TOeU/JgZ4RyCuAJTJjqKUPK4l8K/3hEHnZAOvutg==", null, false, "55c775f6-a757-4b0f-bab9-f71276033acc", false, "proconsulta@hotmail.com.br" });
+
+            migrationBuilder.InsertData(
+                table: "Especialidades",
+                columns: new[] { "Id", "Descricao", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Especialidade médica que trata doenças do coração e do sistema cardiovascular.", "Cardiologia" },
+                    { 2, "Especialidade médica que trata doenças da pele, cabelo e unhas.", "Dermatologia" },
+                    { 3, "Especialidade médica que trata doenças do sistema digestivo.", "Gastroenterologia" },
+                    { 4, "Especialidade médica que trata doenças do sistema nervoso.", "Neurologia" },
+                    { 5, "Especialidade médica que trata doenças e lesões do sistema musculoesquelético.", "Ortopedia" },
+                    { 6, "Especialidade médica que trata de crianças e adolescentes.", "Pediatria" },
+                    { 7, "Especialidade médica que trata de doenças mentais e distúrbios emocionais.", "Psiquiatria" },
+                    { 8, "Especialidade médica que trata doenças dos olhos.", "Oftalmologia" },
+                    { 9, "Especialidade médica que trata do sistema reprodutor feminino.", "Ginecologia" },
+                    { 10, "Especialidade médica que trata do câncer.", "Oncologia" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "8305f33b-5412-47d0-b4ab-17cf6867f2e2", "95433ac4-2fe9-468f-b80d-b05ec3724d1d" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_MedicoId",
+                table: "Agendamentos",
+                column: "MedicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agendamentos_PacienteId",
+                table: "Agendamentos",
+                column: "PacienteId");
+
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -194,11 +328,31 @@ namespace ProConsulta.Migrations
                 column: "NormalizedUserName",
                 unique: true,
                 filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicos_Documento",
+                table: "Medicos",
+                column: "Documento",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicos_EspecialidadeId",
+                table: "Medicos",
+                column: "EspecialidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_Documento",
+                table: "Pacientes",
+                column: "Documento",
+                unique: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
             migrationBuilder.DropTable(
                 name: "AspNetRoleClaims");
 
@@ -215,10 +369,19 @@ namespace ProConsulta.Migrations
                 name: "AspNetUserTokens");
 
             migrationBuilder.DropTable(
+                name: "Medicos");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+
+            migrationBuilder.DropTable(
                 name: "AspNetRoles");
 
             migrationBuilder.DropTable(
                 name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Especialidades");
         }
     }
 }
